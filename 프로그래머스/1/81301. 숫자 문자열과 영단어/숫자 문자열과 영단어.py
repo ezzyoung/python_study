22c6633bd2c670d3487697b34773ea1863d3f191
Jiyoung Lee <laurannie02@naver.com>
2026-02-05 23:21:00 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 9.29 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,14 @@
+def solution(s):
+    d = {0:'zero',
+        1:'one',
+        2:'two',
+        3:'three',
+        4:'four',
+        5:'five',
+        6:'six',
+        7:'seven',
+        8:'eight',
+        9:'nine'}
+    for key, value in d.items():
+        s=s.replace(value,str(key))
+    return int(s)
