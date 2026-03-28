0398f32b27cbe077bc9fcd89bb8f8330c3607006
Jiyoung Lee <laurannie02@naver.com>
2026-03-28 18:02:55 +0900

    [level 1] Title: 과일 장수, Time: 82.81 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,9 +1,3 @@
 def solution(k, m, score):
-    score.sort(reverse=True) #큰 숫자부터 작은 숫자
     answer = 0
-    
-    for i in range(m-1, len(score), m):
-        answer+=score[i]*m
-        
     return answer
-        
